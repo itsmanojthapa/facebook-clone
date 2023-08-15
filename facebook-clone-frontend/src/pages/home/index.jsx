b37ac5d702d52aac5d595a4c941fdc5fc6582855
itsmanojthapa <itsmanojthapa@gmail.com>
2023-08-16 00:20:16 +0530

    test appears to disappear div depending upon where u click

--- facebook-clone-frontend/src/pages/home/index.jsx
+++ facebook-clone-frontend/src/pages/home/index.jsx
@@ -1,11 +1,23 @@
+import { useRef, useState } from "react";
 import Header from "../../components/header";
 import "./style.css";
+import useclickOutSide from "../../helper/clickOutSide";
 
 export default function Home() {
+  const { visible, setVisible } = useState(true);
+  const el = useRef(null);
+  let call = () => {
+    useState(false);
+  };
+  useclickOutSide(el, () => {
+    // setVisible(false);
+    el.current.style.display = "none";
+  });
   return (
     <>
       <Header />
-      <h1>Home Route</h1>
+
+      {!visible && <div className="card" ref={el}></div>}
     </>
   );
 }
