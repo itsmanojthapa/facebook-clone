0110b3d3cf32559d8b1398875d2754a982226d70
itsmanojthapa <itsmanojthapa@gmail.com>
2023-08-16 11:40:49 +0530

    lil change

--- facebook-clone-frontend/src/pages/home/index.jsx
+++ facebook-clone-frontend/src/pages/home/index.jsx
@@ -4,20 +4,17 @@ import "./style.css";
 import useclickOutSide from "../../helper/clickOutSide";
 
 export default function Home() {
-  const { visible, setVisible } = useState(true);
+  const [visible, setVisible] = useState(true);
   const el = useRef(null);
-  let call = () => {
-    useState(false);
-  };
   useclickOutSide(el, () => {
-    // setVisible(false);
-    el.current.style.display = "none";
+    setVisible(false);
+    // el.current.style.display = "none";
   });
   return (
     <>
       <Header />
 
-      {!visible && <div className="card" ref={el}></div>}
+      {visible && <div className="card" ref={el}></div>}
     </>
   );
 }
