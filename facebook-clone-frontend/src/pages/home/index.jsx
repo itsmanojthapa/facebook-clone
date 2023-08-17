b6f2b612c94d9da6aaabff5aff82287e3e584366
itsmanojthapa <itsmanojthapa@gmail.com>
2023-08-18 01:35:39 +0530

    home page responsiveness 1

--- facebook-clone-frontend/src/pages/home/index.jsx
+++ facebook-clone-frontend/src/pages/home/index.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import Header from "../../components/header";
 import "./style.css";
 import useclickOutSide from "../../helpers/clickOutSide";
@@ -9,25 +9,21 @@ import Stories from "../../components/home/stories";
 import CreatePost from "../../components/createPost";
 
 export default function Home() {
-  const [visible, setVisible] = useState(true);
   const user = useSelector((state) => {
     return state.user;
   });
-  const el = useRef(null);
-  useclickOutSide(el, () => {
-    setVisible(false);
-    // el.current.style.display = "none";
-  });
+
   return (
-    <div className="home">
+    <div className="base">
       <Header />
-      <LeftHome user={user} />
-      <div className="home_middle">
-        <Stories />
-        <CreatePost user={user} />
+      <div className="home">
+        <LeftHome user={user} />
+        <div className="home_middle">
+          <Stories />
+          <CreatePost user={user} />
+        </div>
+        <RightHome user={user} />
       </div>
-      <RightHome user={user} />
-      {/* {visible && <div className="card" ref={el}></div>} */}
     </div>
   );
 }
