deac4045185f5d62436807f38d15546f33005143
itsmanojthapa <itsmanojthapa@gmail.com>
2023-09-18 23:30:34 +0530

    Profile Page Left Side flexed scroll

--- facebook-clone-frontend/src/pages/profile/index.jsx
+++ facebook-clone-frontend/src/pages/profile/index.jsx
@@ -20,6 +20,7 @@ import Post from "../../components/post";
 import Photos from "./Photos";
 import Friends from "../../components/post/Friends";
 import Intro from "../../components/intro";
+import { useMediaQuery } from "react-responsive";
 
 export default function Profile({ user, visible, setVisible }) {
   const { username } = useParams();
@@ -84,10 +85,29 @@ export default function Profile({ user, visible, setVisible }) {
       setError(error.response.data.message);
     }
   };
+  const profileTop = useRef(null);
+  const leftSide = useRef(null);
+  const [height, setHeight] = useState();
+  const [leftHeight, setLeftHeight] = useState();
+  const [scrollHeight, setScrollHeight] = useState();
+
+  useEffect(() => {
+    setHeight(profileTop.current.clientHeight + 300);
+    setLeftHeight(leftSide.current.clientHeight);
+    window.addEventListener("scroll", getScroll, { passive: true });
+  }, [loading, scrollHeight]);
+
+  const check = useMediaQuery({
+    query: "(min-width:901px)",
+  });
+
+  const getScroll = () => {
+    setScrollHeight(window.pageYOffset);
+  };
   return (
     <div className="profile">
       <Header page="profile" />
-      <div className="profile_top">
+      <div className="profile_top" ref={profileTop}>
         <div className="profile_container">
           <Cover cover={profile.cover} visitor={visitor} photos={photos} />
           <ProfilePictureInfos
@@ -103,13 +123,22 @@ export default function Profile({ user, visible, setVisible }) {
         <div className="profile_container">
           <div className="bottom_container">
             <PplYouMayKnow />
-            <Intro
-              detailss={profile.details}
-              visitor={visitor}
-              setOthername={setOthername}
-            />
-            <div className="profile_grid">
-              <div className="profile_left">
+
+            <div
+              className={`profile_grid ${
+                check && scrollHeight >= height && leftHeight > 1000
+                  ? "scrollFixed showLess"
+                  : check &&
+                    scrollHeight >= height &&
+                    leftHeight < 1000 &&
+                    "scrollFixed showMore"
+              }`}>
+              <div className="profile_left" ref={leftSide}>
+                <Intro
+                  detailss={profile.details}
+                  visitor={visitor}
+                  setOthername={setOthername}
+                />
                 <Photos
                   userName={userName}
                   token={user.token}
